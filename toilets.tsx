daa1e84bfb333360a5a816aef57e67a34ce77b2e
Carlos Almeida <cosmosdesigner@gmail.com>
2021-04-09 11:31:38 +0100

    first test

---
+++ toilets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="toilets" tilewidth="32" tileheight="32" tilecount="99" columns="9">
+ <image source="../../../Downloads/toilets.png" width="288" height="352"/>
+</tileset>
